58182332f6b8b746d7df60cde407cad411e8ac1c
jose juan <jjcm2004.2015@gmail.com>
2025-02-07 10:35:49 +0100

    poke2

--- React/pokeapp/src/components/RutasPrivadas.jsx
+++ React/pokeapp/src/components/RutasPrivadas.jsx
@@ -1,10 +1,11 @@
-import {Navigate, Outlet} from 'react-router-dom';
-export function RutasPrivadas()
-{
-    let auth = true;
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext"; // 🔹 Asegúrate de tener el contexto de autenticación
 
-    return (
-        auth? <Outlet />: <Navigate to="/"/> 
-    );
+const RutasPrivadas = () => {
+  const { user } = useAuth(); // Obtener el usuario autenticado
 
-}
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default RutasPrivadas;
